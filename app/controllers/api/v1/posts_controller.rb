6465efc6dd0ccc2f1a4ca2093ab32c744ec2ab0f
TakamizawaNoriaki <takamizawa@frembassy.jp>
2023-02-12 07:27:34 +0900

    WIP

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -41,7 +41,8 @@ module Api
       end
 
       def post_params
-        params.require(:post).permit(:title)
+        params.require(:post).permit(:name, :class, :strength, :skill, :dexterity)
+        #params.require(:post).permit(:title)
       end
     end
   end
