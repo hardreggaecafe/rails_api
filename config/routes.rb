b60350f4cf1fdd2e7b1de60ddfab58d2b9c80f4e
hardreggaecafe <takamizawa@frembassy.jp>
2023-03-07 23:20:54 +0900

    WIP: docker ecr config & devise_token_auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+
   # Defines the root path route ("/")
   # root "articles#index"
   namespace 'api' do
     namespace 'v1' do
       resources :posts
+      mount_devise_token_auth_for 'User', at: 'auth'
     end
   end
 end
